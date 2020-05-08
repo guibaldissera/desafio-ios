bf93871237e8d3577df94e1c5bbe7ab1c1776a53
Baldissera <guilhermebaldissera@gmail.com>
2020-05-08 17:30:17 -0300

    Creating and configurating project - Added app icon; - Create initial application scene; - Adding swiftlit.

---
+++ project/Tembici Movies/Scenes/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Tembici Movies
+//
+//  Created by Guilherme Baldissera on 08/05/20.
+//  Copyright © 2020 Guilherme Baldissera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.systemYellow
+    }
+}
---
+++ project/Tembici Movies/Supporting Files/SceneDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  SceneDelegate.swift
+//  Tembici Movies
+//
+//  Created by Guilherme Baldissera on 08/05/20.
+//  Copyright © 2020 Guilherme Baldissera. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Get current window scene
+        guard let windowScene = scene as? UIWindowScene else { return }
+
+        // Update window with window scene
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
+
+        // Update view showed in window
+        window.rootViewController = ViewController()
+
+        // Show view
+        window.makeKeyAndVisible()
+    }
+}
